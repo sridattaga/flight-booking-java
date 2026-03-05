4a57eeb65a76e41d6d797bc8a28b2b7c50d704eb
sridattaga <sivag.soc95@gmail.com>
2026-03-05 23:51:53 +0530

    Change JPA imports from javax to jakarta
    Updated import statements to use Jakarta Persistence.

--- src/main/java/com/dxbair/services/flightbooking/domain/entity/FlightBooking.java
+++ src/main/java/com/dxbair/services/flightbooking/domain/entity/FlightBooking.java
@@ -2,15 +2,15 @@ package com.dxbair.services.flightbooking.domain.entity;
 
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
